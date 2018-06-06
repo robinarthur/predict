b0a5a19d67ceea29bf07eb174536408e90972107
robinarthur <stukka@gmx.de>
2018-06-06 06:38:17 +0200

    import some scrapy things

---
+++ oddsscraper/oddsscraper/spiders/quotes_spider.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  6 06:20:37 2018
+
+@author: robinarthur
+"""
+
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    
+    def start_requests(self):
+        urls = [
+                'http://quotes.toscrape.com/page/1/',
+                'http://quotes.toscrape.com/page/2/'                
+                ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+            
+            
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
